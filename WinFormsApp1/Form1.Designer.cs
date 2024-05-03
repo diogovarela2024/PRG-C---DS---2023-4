a8ef8395ad01a1b9f7d8e285a6428103fad4f96a
diogovarela2024 <diogo.varela@my.istec.pt>
2024-05-03 15:58:45 +0100

    update2

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            this.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(356, 169);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 32);
+            label1.TabIndex = 0;
+            label1.Text = "nome:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(454, 169);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 39);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(356, 252);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 32);
+            label2.TabIndex = 2;
+            label2.Text = "idade";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(454, 245);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 39);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(379, 326);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 32);
+            label3.TabIndex = 4;
+            label3.Text = "NIF";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(444, 310);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(200, 39);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(342, 385);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 32);
+            label4.TabIndex = 6;
+            label4.Text = "Num";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(444, 382);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(200, 39);
+            textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(903, 396);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 8;
+            button1.Text = "Adicionar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(903, 303);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 9;
+            button2.Text = "Salvar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1709, 917);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+    }
+}
